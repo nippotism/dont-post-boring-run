baa1df9d37f937c9ea4fe97a6b0a5a6b92cce70d
Syariful Hanif Setiawan <syarifulhanif@gmail.com>
2025-10-11 06:36:30 +0700

    eror 7

--- app/activities/[id]/TemplatePage.tsx
+++ app/activities/[id]/TemplatePage.tsx
@@ -221,7 +221,7 @@ export default function ActivityTemplatePage({
                         link.click();
 
                         //post to record
-                        fetch(`${import.meta.env.VITE_API_URL}/api/records`, {
+                        fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/records`, {
                           method: "POST",
                           headers: {
                             "Content-Type": "application/json",
