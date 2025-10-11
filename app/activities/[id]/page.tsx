ba06223082da0f30ebb252e8a027601e3245b9b7
Syariful Hanif Setiawan <syarifulhanif@gmail.com>
2025-10-11 13:51:40 +0700

    ts

--- app/activities/[id]/page.tsx
+++ app/activities/[id]/page.tsx
@@ -1,6 +1,4 @@
-// app/activities/[id]/page.tsx
-import { cookies } from "next/headers";
-import ActivityTemplatePage from "./TemplatePage";
+
 import { Metadata } from "next";
 
 export const dynamic = "force-dynamic"; // agar tidak dibuild statis
@@ -17,18 +15,5 @@ export default async function ActivityTemplateRoute({
 }) {
 
   const { id } = await params;
-  const cookieStore = await cookies();
-  const stravaSession = cookieStore.get("strava_session")?.value;
-
-  let athleteId: string | null = null;
-  if (stravaSession) {
-    try {
-      const sessionData = JSON.parse(stravaSession);
-      athleteId = sessionData.id;
-    } catch {
-      athleteId = stravaSession; 
-    }
-  }
-
-  return <ActivityTemplatePage id={id} athleteId={athleteId} />;
+  return <div>Activity ID: {id}</div>;
 }
